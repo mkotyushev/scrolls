80a20312ad01d76f164ac056dc9355dfac2137d8
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-05-15 06:38:23 +0000

    Remove fake parts

--- src/model/fake_model.py
+++
@@ -1,7 +0,0 @@
-from lightning.pytorch.demos.boring_classes import DemoModel
-
-
-class FakeModel(DemoModel):
-    def configure_optimizers(self):
-        print("⚡", "using FakeModel", "⚡")
-        return super().configure_optimizers()
