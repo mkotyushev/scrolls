186f3b2d324255450003e5dbe1ec3e44b3d31d24
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-06-03 20:17:22 +0000

    Update scaling script

--- src/scripts/z_shift_scale.py
+++ src/scripts/z_shift_scale.py
@@ -10,7 +10,7 @@ from scipy.ndimage import geometric_transform
 from src.data.datamodules import read_data
 from src.data.datasets import build_z_shift_scale_maps
 
-# Usage: python src/scripts/z_shift_scale.py --input_dir /workspace/data/fragments_downscaled_2 --output_dir /workspace/data/fragments_downscaled_2_z_shift_scale
+# Usage: python src/scripts/z_shift_scale.py --input_dir /workspace/data/fragments/train/2 --output_dir /workspace/data/fragments_z_shift_scale/train/2 --patch_size 256 --z_start 20 --z_end 44
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -23,6 +23,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--input_dir', type=Path, required=True)
 parser.add_argument('--output_dir', type=Path, required=True)
 parser.add_argument('--patch_size', type=int, default=128)
+parser.add_argument('--z_start', type=int, default=None)
+parser.add_argument('--z_end', type=int, default=None)
 args = parser.parse_args()
 
 # Copy all files (except .tif) to output directory keeping the same structure
@@ -37,63 +39,59 @@ for path in args.input_dir.glob('**/*'):
         path_out.write_bytes(path.read_bytes())
 
 # Read data
-fragment_pathes = sorted([
-    str(path) for path in (args.input_dir / 'train').glob('*')
-    if path.is_dir()
-])
-logger.info(fragment_pathes)
+volumes, scroll_masks, ir_images, ink_masks, subtracts, divides = \
+    read_data([args.input_dir])
 
-for fragment_path in fragment_pathes:
-    volumes, scroll_masks, ir_images, ink_masks, subtracts, divides = \
-        read_data([fragment_path])
+# Build z shift and scale maps
+z_shifts, z_scales = build_z_shift_scale_maps(
+    pathes=[args.input_dir],
+    volumes=[volumes[0]],
+    scroll_masks=[scroll_masks[0]],
+    subtracts=[subtracts[0]],
+    divides=[divides[0]],
+    z_start=0,
+    crop_z_span=8,
+    mode='volume_mean_per_z', 
+    normalize='minmax', 
+    patch_size=(args.patch_size, args.patch_size),
+    sigma=0.5,
+)
 
-    # Build z shift and scale maps
-    z_shifts, z_scales = build_z_shift_scale_maps(
-        pathes=[fragment_path],
-        volumes=[volumes[0]],
-        scroll_masks=[scroll_masks[0]],
-        subtracts=[subtracts[0]],
-        divides=[divides[0]],
-        z_start=0,
-        crop_z_span=8,
-        mode='volume_mean_per_z', 
-        normalize='minmax', 
-        patch_size=(args.patch_size, args.patch_size),
-        sigma=0.5,
+def z_shift_scale_map(x):
+    shift, scale = z_shifts[0][x[0], x[1]], z_scales[0][x[0], x[1]]       
+    z = (x[2] - shift) / scale  # assuming non-zero scale
+    return (
+        x[0], 
+        x[1], 
+        z
     )
 
-    def z_shift_scale_map(x):
-        shift, scale = z_shifts[0][x[0], x[1]], z_scales[0][x[0], x[1]]       
-        z = (x[2] - shift) / scale  # assuming non-zero scale
-        return (
-            x[0], 
-            x[1], 
-            z
-        )
+# Apply z shift and scale maps
+volume_transformed = geometric_transform(
+    volumes[0],
+    z_shift_scale_map,
+    order=1,
+)
 
-    # Apply z shift and scale maps
-    volume_transformed = geometric_transform(
-        volumes[0],
-        z_shift_scale_map,
-        order=1,
+# Convert to uint16
+volume_transformed = volume_transformed.astype(np.uint16)
+
+# Get z range
+z_start, z_end = 0, volume_transformed.shape[2]
+if args.z_start is not None:
+    z_start = z_start
+if args.z_end is not None:
+    z_end = z_end
+
+# Save transformed volume
+for z in range(z_start, z_end):
+    path_out = \
+        args.output_dir / \
+        'surface_volume' / f'{z:02}.tif'
+    logger.info(f'Saving layer {z} to {path_out}')
+    
+    path_out.parent.mkdir(parents=True, exist_ok=True)
+    cv2.imwrite(
+        str(path_out),
+        volume_transformed[:, :, z],
     )
-
-    # Convert to uint16
-    volume_transformed = volume_transformed.astype(np.uint16)
-
-    # Save transformed volume
-    for z in range(volume_transformed.shape[2]):
-        path_out = \
-            args.output_dir / \
-            Path(fragment_path).relative_to(args.input_dir) / \
-            'surface_volume' / f'{z:02}.tif'
-        logger.info(f'Saving layer {z} to {path_out}')
-        
-        path_out.parent.mkdir(parents=True, exist_ok=True)
-        cv2.imwrite(
-            str(path_out),
-            volume_transformed[:, :, z],
-        )
-
-    del volumes, volume_transformed
-    gc.collect()
