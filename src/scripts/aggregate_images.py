444b16882e87fea4824f0bd6f022a516a9213731
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-06-06 05:30:33 +0000

    Update aggregation script

--- src/scripts/aggregate_images.py
+++ src/scripts/aggregate_images.py
@@ -1,4 +1,5 @@
 import argparse
+import gc
 import logging
 import os
 import cv2
@@ -19,7 +20,7 @@ logging.basicConfig(
 
 parser = argparse.ArgumentParser(description='Downscale dataset')
 parser.add_argument('proba_dir', type=Path, help='proba directory')
-parser.add_argument('output_path', type=Path, help='output filepath')
+parser.add_argument('output_dir', type=Path, default='.', help='output directory')
 args = parser.parse_args()
 
 ids = sorted(list(set([path.stem.split('_')[0] for path in args.proba_dir.glob('*.png')])))
@@ -30,16 +31,22 @@ for id_ in ids:
 
     logger.info(f'Processing {id_}, proba_pathes: {proba_pathes}...')
 
-    proba = [
-        cv2.imread(str(path), cv2.IMREAD_GRAYSCALE) / 255
-        for path in proba_pathes
-    ]
+    proba = None
+    for path in proba_pathes:
+        gc.collect()
+        if proba is None:
+            proba = np.array(cv2.imread(str(path), cv2.IMREAD_GRAYSCALE)).astype(np.uint32)
+        else:
+            proba += np.array(cv2.imread(str(path), cv2.IMREAD_GRAYSCALE)).astype(np.uint32)
+    proba = proba.astype(np.float64) / (255 * len(proba_pathes))
+    probas.append(proba)
 
-    proba_agg = np.stack(proba, axis=0).mean(axis=0)
-    probas.append(proba_agg)
+# Save images
+for i, (id_, proba) in enumerate(zip(ids, probas)):
+    cv2.imwrite(str(args.output_dir / f'{id_}_mean.png'), (proba * 255).astype(np.uint8))
 
 # Save
-with open(args.output_path, 'w') as f:
+with open(args.output_dir / 'submission.csv', 'w') as f:
     print("Id,Predicted", file=f)
     for i, (id_, proba) in tqdm(enumerate(zip(ids, probas))):
         starts_ix, lengths = rle(proba)
